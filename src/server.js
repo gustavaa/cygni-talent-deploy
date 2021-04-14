b54316b5ea187f29c6d55a5867698c9b79885849
Gustav Aaro <gustav.aaro@gmail.com>
2021-04-14 17:27:56 +0200

    Increment build number

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const http = require('http');
 
 function createServer(config = {}) {
-  const { buildNumber = '1' } = config;
+  const { buildNumber = 'n/a' } = config;
 
   const server = http.createServer((req, res) => {
     console.log(`Received request ${req.method.toUpperCase()} ${req.url}`);
     res.writeHead(200, 'OK');
-    res.write(Buffer.from(`Hello Crazy World! Build number ${buildNumber}\n`));
+    res.write(
+      Buffer.from(`Hello World! New stuffs! Build number ${buildNumber}\n`)
+    );
     res.end();
   });
 
