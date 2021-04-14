d52cda7782e27eed7da7a2b1c74f093d1ce52b9c
Gustav Aaro <gustav.aaro@gmail.com>
2021-04-14 16:46:13 +0200

    Run lint

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const http = require("http");
 
 function createServer(config = {}) {
-  const { buildNumber = 'n/a' } = config;
+  const { buildNumber = "n/a" } = config;
 
   const server = http.createServer((req, res) => {
     console.log(`Received request ${req.method.toUpperCase()} ${req.url}`);
-    res.writeHead(200, 'OK');
+    res.writeHead(200, "OK");
     res.write(
       Buffer.from(`Hello World! ASDASDAS Build number ${buildNumber}\n`)
     );
